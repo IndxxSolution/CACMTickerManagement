f2b9e49094b70fa28bbda9b900f929739bf88d66
kashish-indxx <61489843+kashish-indxx@users.noreply.github.com>
2020-03-09 11:27:57 +0530

    assigning

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,6 +3,8 @@ from .views  import *
 from rest_framework_simplejwt import views as jwt_views
 from rest_framework.routers import DefaultRouter
 
+
+
 router = DefaultRouter()
 router.register(r'register', Register, basename='register') 
 router.register(r'dividend', Dividend, basename='dividend') 
